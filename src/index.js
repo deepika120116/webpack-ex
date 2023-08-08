38e41c9c4c1390336e57bc19ccfd6c69b0bc46b0
Deepika Somasundaram <sdeepikas88@gmail.com>
2023-08-08 14:34:37 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Welcome from "./components/welcome";
+
+const root = document.getElementById('root');
+ReactDOM.createRoot(root).render(<Welcome />);
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, 'dist')
+    },
+    mode: "none",
+    plugins: [
+        new htmlWebpackPlugin({
+            template:path.join(__dirname,'public','index.html')
+        }),
+    ],
+    devServer: {
+        static: {
+            directory:path.join(__dirname,'dist')
+        },
+        port:3000
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use:["babel-loader"]
+            },
+        ],
+    },
+    resolve: {
+        extensions:["*",".js",".jsx"]
+    }
+};
